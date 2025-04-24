531f27da32ae7865469f056038e4963d64a73783
dfedotov <dfedotov@lanta-net.ru>
2025-04-24 11:35:57 +0300

    Add `port` argument, fix code formatting

--- wb-mir/ir_backup/write_roms.pl
+++ wb-mir/ir_backup/write_roms.pl
@@ -1,40 +1,30 @@
 #!/usr/bin/perl
 use 5.010;
 
-# ./write_rom.pl "directory" "modbus-address" - write banks to the device
+$dir = $ARGV[0];
+$ARGV[4] = '/dev/ttyRS485-1' unless defined $ARGV[4];
 
-$dir=$ARGV[0];
-
-print "Stoping wb-mqtt-serial \n";
+print "Stopping wb-mqtt-serial \n";
 `systemctl stop wb-mqtt-serial`;
 
-
 foreach $i (0..$ARGV[3]-1) {
-
-$reg=5200 + $i;
-`modbus_client --debug -mrtu -b$ARGV[2] -pnone -o 700 -s2 /dev/ttyRS485-1 -a$ARGV[1] -t0x05 -r $reg 0`;
-sleep(2);
-
+    $reg = 5200 + $i;
+    `modbus_client --debug -mrtu -b$ARGV[2] -pnone -o 700 -s2 $ARGV[4] -a$ARGV[1] -t0x05 -r $reg 0`;
+    sleep(2);
 }
 
-
-
 foreach $i (0..$ARGV[3]-1) {
-
-$reg=5200 + $i;
-#`modbus_client --debug -mrtu -pnone -o 700 -s2 /dev/ttyRS485-1 -a2 -t0x05 -r $reg 0`;
-#sleep(2);
-`modbus_client --debug -mrtu -b$ARGV[2] -pnone -o 700 -s2 /dev/ttyRS485-1 -a$ARGV[1] -t0x05 -r $reg 1`;
-$j=$i+1;
-sleep (20); print "->$j";
-
-`./putbuffer.pl ./$dir/rom_$j.ir $ARGV[1]`;
-print "<-";
-`modbus_client --debug -mrtu -b$ARGV[2] -pnone -o 700 -s2 /dev/ttyRS485-1 -a$ARGV[1] -t0x05 -r $reg 0`;
-sleep (2);
+    $reg = 5200 + $i;
+    `modbus_client --debug -mrtu -b$ARGV[2] -pnone -o 700 -s2 $ARGV[4] -a$ARGV[1] -t0x05 -r $reg 1`;
+    $j = $i + 1;
+    sleep(20); print "->$j";
+    `./putbuffer.pl ./$dir/rom_$j.ir $ARGV[1]`;
+    print "<-";
+    `modbus_client --debug -mrtu -b$ARGV[2] -pnone -o 700 -s2 $ARGV[4] -a$ARGV[1] -t0x05 -r $reg 0`;
+    sleep(2);
 }
 
-print "\n Starting wb-mqtt-serial \n";
+print "\nStarting wb-mqtt-serial \n";
 `systemctl start wb-mqtt-serial`;
 
-exit();
+exit;
